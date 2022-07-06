a868159b5c8057cfc1beba821126b9d373ee1256
natanboanafina <boanafina.natan@gmail.com>
2022-07-06 18:13:53 -0300

    Home screen + NavOptions created and some styles added

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
+import React from "react";
+import HomeScreen from "./screens/HomeScreen";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Let's build Uber!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
