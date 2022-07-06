2a49c9d1715356a1151ccc7252762656c0d97b69
natanboanafina <boanafina.natan@gmail.com>
2022-07-06 16:41:26 -0300

    Redux edited - Redux toolkit

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from 'react-redux';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function App() {
   return (
-    <Provider>
+    <Provider store={store}>
       <View style={styles.container}>
         <Text>Let's build Uber!</Text>
-        <StatusBar style='auto' />
+        <StatusBar style="auto" />
       </View>
     </Provider>
   );
@@ -16,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
